73e8e3434777bcf758cd4022d6582c47a53b2d98
Hossein Mohammadzadeh <4hmzjob@gmail.com>
2024-10-16 00:31:14 +0330

    V.0.3.2 add loader in submit button in form

--- src/components/element/FormButton.jsx
+++ src/components/element/FormButton.jsx
@@ -1,12 +1,24 @@
 import { useFormStatus } from "react-dom";
+import { FiLoader } from "react-icons/fi";
 
 function FormButton() {
   const { pending } = useFormStatus();
 
   return (
-    <button type="submit" className="button1 w-28" disabled={pending}>
-      ورود
-    </button>
+    <>
+      {pending ? (
+        <button
+          className="button1 w-28 flex items-center justify-center"
+          disabled
+        >
+          <FiLoader fontSize="1.5rem" />
+        </button>
+      ) : (
+        <button type="submit" className="button1 w-28">
+          ورود
+        </button>
+      )}
+    </>
   );
 }
 
