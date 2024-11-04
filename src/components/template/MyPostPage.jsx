a6b2ee9f2fd31c6d0658e1e65fda466d764dbc68
Hossein Mohammadzadeh <4hmzjob@gmail.com>
2024-11-04 17:07:39 +0330

    V.0.5.3 build bug fixed

--- src/components/template/MyPostPage.jsx
+++ src/components/template/MyPostPage.jsx
@@ -1,7 +1,7 @@
 import MyPostCard from "@/module/MyPostCard";
 
 function MyPostPage({ posts }) {
-  if (posts?.length === 0)
+  if (!posts || posts.length === 0)
     return (
       <div className="w-full text-center p-2 bg-line rounded">
         هنوز آکهی ثبت نشده!
@@ -15,7 +15,7 @@ function MyPostPage({ posts }) {
       </div>
 
       <div className="w-full flex flex-col items-center justify-start gap-2 sm:flex-row sm:flex-wrap sm:justify-center">
-        {posts.map((item) => (
+        {posts?.map((item) => (
           <MyPostCard data={item} key={item._id} />
         ))}
       </div>
