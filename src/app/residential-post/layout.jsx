08ee52710f9b9739562efd3f2230e4c72cbc7e26
Hossein Mohammadzadeh <4hmzjob@gmail.com>
2024-11-13 01:07:04 +0330

    V.0.7.1.1 deploy bug fix 2

--- src/app/residential-post/layout.jsx
+++ src/app/residential-post/layout.jsx
@@ -1,11 +1,14 @@
 import ResidentialSidebar from "@/layout/ResidentialSidebar";
+import { Suspense } from "react";
 
 function ResidentialLayout({ children }) {
   return (
     <>
       <div className="w-full p-1 flex flex-col items-center justify-start gap-2 sm:flex-row sm:items-start sm:justify-between">
         <div className="w-full rounded sm:shadow sm:w-60 sm:pm-6">
-          <ResidentialSidebar />
+          <Suspense fallback={<div>Loading... </div>}>
+            <ResidentialSidebar />
+          </Suspense>
         </div>
 
         <span className="w-full h-px bg-line sm:hidden"></span>
