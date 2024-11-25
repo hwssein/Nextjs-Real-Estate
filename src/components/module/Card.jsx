1a00e6a5cb1f91e0a5f3b3ab4efa301d973d07eb
Hossein Mohammadzadeh <4hmzjob@gmail.com>
2024-11-25 15:57:30 +0330

    V.1.1.6.2 add image in cards

--- src/components/module/Card.jsx
+++ src/components/module/Card.jsx
@@ -1,28 +1,49 @@
 import Link from "next/link";
 import { sp } from "@/utils/replaceNumber";
 import icon from "@/constants/icons";
+import Image from "next/image";
 
 function Card({ data }) {
   return (
     <>
-      <div className="w-full flex flex-col items-start justify-start gap-1 border border-line rounded p-2">
-        <div className="w-full flex items-start justify-start gap-2">
-          <span className="text-primary">{icon[data.category]}</span>
-          <span className="text-lg h-16 break-words w-full">
-            {data.postTitle}
-          </span>
+      <Link href={`/residential-details/${data._id}`}>
+        <div className="w-full flex items-start justify-between gap-1 border border-line rounded p-2">
+          <div className="w-full flex flex-col items-start justify-start gap-1">
+            <div className="w-full flex items-start justify-start gap-2">
+              <span className="text-primary">{icon[data.category]}</span>
+              <span className="text-lg h-20 break-words w-full">
+                {data.postTitle}
+              </span>
+            </div>
+
+            <span>{data.address}</span>
+
+            <span>{sp(data.price)} تومان</span>
+          </div>
+
+          {data?.image.length !== 0 ? (
+            <div className="w-28">
+              <Image
+                src={data?.image[0]}
+                width={400}
+                height={300}
+                alt={`card-${data._id}`}
+                className="w-full"
+              ></Image>
+            </div>
+          ) : (
+            <div className="w-28">
+              <Image
+                src="/image/image-unavailable.png"
+                width={400}
+                height={300}
+                alt={`card-${data._id}`}
+                className="w-full"
+              ></Image>
+            </div>
+          )}
         </div>
-
-        <span>{data.address}</span>
-
-        <span>{sp(data.price)} تومان</span>
-
-        <div className="w-full flex items-center justify-end">
-          <Link href={`/residential-details/${data._id}`}>
-            <button className="text-primary p-1">مشاهده آگهی</button>
-          </Link>
-        </div>
-      </div>
+      </Link>
     </>
   );
 }
